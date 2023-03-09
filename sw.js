98d69ea2adc8c9aa583d3b151f0611624103c157
2tu <enum@foxmail.com>
2023-03-09 20:21:43 +0800

    add new run

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b72dbb5f175550e04336.js"
+    "url": "webpack-runtime-e7c66905b6e3e0fdc227.js"
   },
   {
     "url": "framework-8092182b2c8a45123cff.js"
   },
   {
-    "url": "app-651e8e56dda48af2ed16.js"
+    "url": "app-ac9625ce9bd2b4ed3272.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4aad87ab2813d9b15416eb351309da28"
+    "revision": "57aeff3938e2d8bb0e7e489dc869cb6d"
   },
   {
     "url": "polyfill-14854c9902868fed1421.js"
@@ -154,7 +154,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-651e8e56dda48af2ed16.js`))) {
+  if (!resources || !(await caches.match(`/app-ac9625ce9bd2b4ed3272.js`))) {
     return await fetch(event.request)
   }
 
